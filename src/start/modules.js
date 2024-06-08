fce4198e9ed366c7394782bd81d03eba2f45aef6
ulugbekerkinovich2 <ulugbek9015y@gmail.com>
2024-06-08 16:37:49 +0500

    fixed ico and background image

--- src/start/modules.js
+++ src/start/modules.js
@@ -5,15 +5,16 @@ const usersRoute = require("../routes/users.route");
 const todoRoute = require("../routes/todo.route");
 const indexRoute = require("../routes/index.route");
 const isAuthMiddleware = require("../middlewares/is-auth.middleware");
-
+const path = require("path");
 const modules = (app, express) => {
   app.use(cors());
   app.use(fileUpload());
   app.set("view engine", "ejs");
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(express.static(`${process.cwd()}/uploads`));
+  // app.use(express.static(path.join(__dirname, "../public")));
   app.use(express.static(`${process.cwd()}/public`));
+  app.use(express.static(`${process.cwd()}/images`));
   app.use("/api/auth", authRoute);
   app.use("/api/users", usersRoute);
   app.use("/api/todos", todoRoute);
